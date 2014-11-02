667bc8047d8ce7b2bd617be9955b4b71e0eab301
ritou <ritou.06@gmail.com>
2014-11-02 04:58:54 +0000

    separate logic from endpoint

--- lib/Net/OATH/Server/Lite/Endpoint/Login.pm
+++ lib/Net/OATH/Server/Lite/Endpoint/Login.pm
@@ -10,16 +10,8 @@ use Plack::Request;
 use Params::Validate;
 use JSON::XS qw/decode_json encode_json/;
 
-use Authen::OATH;
+use Net::OATH::Server::Lite::Login;
 use Net::OATH::Server::Lite::Error;
-
-my %DIGEST_MAP = (
-    SHA1 => q{Digest::SHA1},
-    MD5 => q{Digest::MD5},
-    # TODO: Support SHA256, SHA512
-    # SHA256 => q{Digest::SHA256},
-    # SHA512 => q{Digest::SHA512},
-);
 
 sub new {
     my $class = shift;
@@ -85,35 +77,7 @@ sub handle_request {
         };
         Net::OATH::Server::Lite::Error->throw() unless $params;
 
-        # Params
-        my $id = $params->{id} or
-            Net::OATH::Server::Lite::Error->throw(
-                description => q{missing id},
-            );
-
-        my $password = $params->{password} or 
-            Net::OATH::Server::Lite::Error->throw(
-                description => q{missing password},
-            );
-
-        # obtain user model
-        my $user = $data_handler->select_user($id) or
-            Net::OATH::Server::Lite::Error->throw(
-                code => 404,
-                description => q{invalid id},
-            );
-        Net::OATH::Server::Lite::Error->throw(
-            code => 500,
-            error => q{server_error},
-        ) unless $user->isa(q{Net::OATH::Server::Lite::Model::User});
-
-        my $timestamp = ($params->{timestamp}) ? $params->{timestamp} : time();
-        my $counter = (defined $params->{counter}) ? $params->{counter} : $user->counter;
-        my $is_valid = $self->is_valid_password($password, $user, $timestamp, $counter);
-        if ($user->type eq q{hotp} and !defined $params->{counter}) {
-            $user->counter($user->counter + 1);
-            $data_handler->update_user($user);
-        }
+        my ($is_valid, $user) = Net::OATH::Server::Lite::Login->is_valid_user($data_handler, $params);
 
         if ($is_valid) {
             my $response_params = {
@@ -147,33 +111,6 @@ sub handle_request {
             die $_;
         }
     };
-}
-
-sub is_valid_password {
-    my ($self, $password, $user, $timestamp, $counter) = @_;
-
-    # generate password
-    my $oath = 
-        Authen::OATH->new( 
-            digits => $user->digits,
-            digest => _digest_for_oath($user->algorithm),
-            timestep => $user->period,
-        );
-
-    if ($user->type eq q{totp}) {
-        # TOTP
-        return ($password eq $oath->totp($user->secret, $timestamp));
-    } else {
-        # HOTP
-        return ($password eq $oath->hotp($user->secret, $counter));
-    }
-
-    return 1;
-}
-
-sub _digest_for_oath {
-    my $algorithm = shift;
-    return ($DIGEST_MAP{$algorithm}) ? $DIGEST_MAP{$algorithm} : q{Digest::SHA1};
 }
 
 1;
